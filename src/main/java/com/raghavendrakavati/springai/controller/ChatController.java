5ccea8b14b5982930004a1ac0ac1f8db9b5b6236
Kavati Ragahvendra <kavatiragheavendra123@gmail.com>
2026-05-01 23:29:45 +0530

    Changes in Spring AI

---
+++ src/main/java/com/raghavendrakavati/springai/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.raghavendrakavati.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+}
